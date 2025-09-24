a8f85fac7d55c9e50a19588322f0ea0ea8bf00d3
anup2003D <anup03dutta.bapon@gmail.com>
2025-09-24 13:20:03 +0530

    commit 3 open app db

--- Backend/db.py
+++ Backend/db.py
@@ -1,3 +1,19 @@
 import sqlite3
 
 conn = sqlite3.connect("jarvis.db")
+cursor = conn.cursor()
+
+query = "CREATE TABLE IF NOT EXISTS sys_command(id integer primary key, name VARCHAR(100), path VARCHAR(1000))"
+cursor.execute(query)
+query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), path VARCHAR(1000))"
+cursor.execute(query)
+
+query = "INSERT INTO sys_command VALUES(null,'Whatsapp', 'C:\\Users\\Anup0\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Chrome Apps\\WhatsApp Web.lnk')"
+cursor.execute(query)
+conn.commit()
+'''query = "INSERT INTO web_command VALUES(null,'whatsapp', 'C:\\Users\\Anup0\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Chrome Apps\\WhatsApp Web.lnk')"
+cursor.execute(query)
+conn.commit()'''
+'''query = "INSERT INTO web_command VALUES(null,'instagram', 'https://www.instagram.com/')"
+cursor.execute(query)
+conn.commit()'''
--- Backend/feature.py
+++ Backend/feature.py
@@ -13,6 +13,7 @@ def say_what_again():
     playsound(music_dir2)'''
     
     
+import webbrowser
 import eel
 import pygame
 from Backend.config import ASSISSTANT_NAME
@@ -20,8 +21,12 @@ import os
 from Backend.command import speak
 import re
 import pywhatkit as kit
+import sqlite3
+conn = sqlite3.connect("jarvis.db")
+cursor = conn.cursor()
 
 pygame.mixer.init()
+
 @eel.expose
 def play_assistant_sound():
     sound_file = r"C:\Users\Anup0\Data Science Projects\Data Science\Jarvis\Frontend\assets\audio\WOW.mp3"
@@ -34,11 +39,42 @@ def openCommand(query):
     query=query.replace("open","")
     query.lower()
     
-    if query!="":
+    app_name=query.strip()
+    
+    if app_name!="":
+        try:
+            cursor.execute('SELECT path FROM sys_command WHERE name IN (?)',(app_name,))
+            results = cursor.fetchall()
+            print(results)
+
+            if len(results)!=0:
+                speak("Opening "+query)
+                os.startfile(results[0][0]) 
+
+            '''elif len(results)==0:
+                cursor.execute('SELECT url FROM web_command WHERE name IN (?)',(app_name,))
+                results = cursor.fetchall()
+                print(results)
+
+                if len(results)!=0:
+                    speak("Opening "+query)
+                    webbrowser.open(results[0][0])
+
+                else:
+                    speak("Opening "+query)
+                    try:
+                        os.system('start '+query)
+                    except:
+                        speak("not found")'''
+        except:
+            speak("some thing went wrong")
+
+
+    '''if query!="":
         speak("Opening " + query)
         os.system("start "+(query))
     else:
-        speak("Please specify what to open.")
+        speak("Please specify what to open.")'''
         
 def PlayYoutube(query):
     search_term=extract_yt_term(query)
